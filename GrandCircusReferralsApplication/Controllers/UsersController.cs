614f40635e617a7419e606d899a67dd20132fb49
coloritoj <coloritoj@gmail.com>
2023-01-29 11:00:29 -0500

    Add project files.

---
+++ GrandCircusReferralsApplication/Controllers/UsersController.cs
@@ -0,0 +1,17 @@
+﻿using GrandCircusReferralsApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GrandCircusReferralsApplication.Controllers
+{
+    public class UsersController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            HttpClient client = new HttpClient() { BaseAddress = new Uri("https://localhost:7021") };
+            var response = await client.GetAsync("/api/Users");
+            List<BaseUser> users = await response.Content.ReadFromJsonAsync<List<BaseUser>>();
+
+            return View(users);
+        }
+    }
+}
---
+++ GrandCircusReferralsApplication/Models/BaseUser.cs
@@ -0,0 +1,27 @@
+﻿namespace GrandCircusReferralsApplication.Models
+{
+    public class BaseUser
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Bootcamp { get; set; }
+
+        public int GraduationFlag { get; set; }
+
+        public int GraduationYear { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public string EmploymentStatus { get; set; }
+
+        public string Employer { get; set; }
+
+        public int InterestFlag { get; set; }
+
+        public string ApplicationStatus { get; set; }
+    }
+}
